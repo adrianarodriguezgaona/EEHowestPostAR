44378c837894152732979679d796122343aebc0b
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2020-01-09 21:36:26 +0100

    properties add to Delivery class

--- HoWestPost.Domain/Delivery.cs
+++ HoWestPost.Domain/Delivery.cs
@@ -6,16 +6,58 @@ namespace HoWestPost.Domain
 {
     public class Delivery
     {
-        public PackageType PackageType { get; private set; }
-        public int TravelTime { get; private set; }
-        public bool IsPrior { get; private set; }
+        private PackageType packageType;
 
-        public Delivery(PackageType packageType, int travelTimeToDestination, bool isPriorOrNot)
+        public PackageType PackageType
         {
-            this.PackageType = packageType;
-            this.TravelTime = travelTimeToDestination;
-            this.IsPrior = isPriorOrNot;
+            get { return packageType; }
         }
+
+        private int travelTime;
+
+        public int TravelTime
+        {
+            get { return travelTime; }
+           
+        }
+
+        private bool isPrior;
+
+        public bool IsPrior
+        {
+            get { return isPrior; }
+           
+        }
+
+        private string priorString;
+
+        public string PriorString
+        {
+            get { return priorString; }
+           
+        }
+
+        private decimal factor = 0M;
+
+        public  decimal Factor
+        {
+            get { return factor; }
+           
+        }
+
+
+
+        public Delivery(PackageType packageType, int travelTimeToDestination, bool isPriorOrNot, string priorString, decimal factor)
+        {
+            this.packageType = packageType;
+            this.travelTime = travelTimeToDestination;
+            this.isPrior = isPriorOrNot;
+            this.priorString = priorString;
+            this.factor = factor;
+
+        }
+
+
     }
 
 }
