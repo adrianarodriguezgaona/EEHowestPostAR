861d67a9d83d0ba7cd6860ccf917cc9c90e12c36
Adriana Rodriguez <adriana.rodriguez.gaona@student.howest.be>
2020-01-09 21:38:04 +0100

    Thread.sleep with 100 to go faster , stop to stop after closing window

--- HoWestPost.Domain/DeliveryProcessor.cs
+++ HoWestPost.Domain/DeliveryProcessor.cs
@@ -4,25 +4,57 @@ using System.Threading;
 
 namespace HoWestPost.Domain
 {
-    public delegate void PakketSendingEventHandler(object sender);
+    public delegate void PakketSendingEventHandler(object sender, int passedTime, int remainingTime, int totalTimeSendingInt);
     public class DeliveryProcessor
     {
         //TODO proces deliveries...  
-        private Delivery currentDelivery;
+        private int passedTime;
+       
         private Thread senderThread;
-        private bool SendingIsRunning;
+        private bool isSending;
+
+        private decimal totalTimeSending = 0M;
+
+        private int remainingTime;
 
        
-        public event PakketSendingEventHandler Delivering;
 
 
-        public void StartSending(object sender)
+        private Delivery currentDelivery;
+        public Delivery CurrentDelivery
+        {
+            get { return currentDelivery; }
+            set { currentDelivery = value; }
+        }
+
+
+        private int totalTimeSendingInt = 0;
+        //public decimal TotalTimeSending
+        //{
+        //    get { return totalTimeSending; }
+        //}
+
+
+        public event PakketSendingEventHandler OnDelivering;
+        public event PakketSendingEventHandler OnSend;
+
+        public DeliveryProcessor()
+        {
+            passedTime = 0;
+            isSending = false;   
+            //if (currentDelivery != null)
+            //{ 
+            //totalTimeSending = currentDelivery.TravelTime / 10 * currentDelivery.Factor;
+            //totalTimeSendingInt = Decimal.ToInt32(totalTimeSending);
+            //}
+        }
+
+        public void StartSending()
         {
             if (senderThread == null)
             {
-                SendingIsRunning = true;
+                isSending = true;
                 senderThread = new Thread(() => { SendingProcedure(); });
-
               
                 senderThread.IsBackground = true;
 
@@ -32,13 +64,44 @@ namespace HoWestPost.Domain
 
         private void SendingProcedure()
         {
-            throw new NotImplementedException();
+            //wordt uitgevoerd in subthread
+            while (isSending)
+            {
+                Send();
+            }
+        }
+
+        private void Send()
+        {
+            Thread.Sleep(100);
+
+           
+                if(passedTime < totalTimeSendingInt) { 
+                passedTime ++;
+                }
+                totalTimeSending = currentDelivery.TravelTime * currentDelivery.Factor;
+                totalTimeSendingInt = Decimal.ToInt32(totalTimeSending);
+                remainingTime = totalTimeSendingInt - passedTime;
+           
+                OnDelivering?.Invoke(this, passedTime, remainingTime, totalTimeSendingInt);
+                 
+        }
+
+        public void Stop()
+        {
+           
+
+            isSending = false;
+            senderThread = null;
         }
 
         public void AddToList(object sender)
         {
 
         }
+
+
+
     }
 
 
